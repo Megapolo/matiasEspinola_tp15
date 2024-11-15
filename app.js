e14ecd7d980e7994051a1fb8b493d3d020595a2d
Megapolo <espinolamatias37@gmail.com>
2024-11-14 22:36:49 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const { stringify } = require("querystring")
+const pelis = require("./peliculas.js")
+const fs = require('fs')
+let mensaje = fs.readFileSync("./mensaje.txt")
+
+for(let i = 0; i < pelis.length; i++){
+    console.log(`Nombre: ${pelis[i].id}`)
+    console.log(`Rating: ${pelis[i].rating}`)
+    console.log(`Premios: ${pelis[i].awards}`)
+    console.log(`Duración: ${pelis[i].length}`)
+    console.log(`Precio: ${pelis[i].price}`)
+    console.log(`Generos: ${pelis[i].genre}`)
+    console.log("--------------------------------------");
+}
+
+mensaje = mensaje.toString()
+
+console.log(mensaje)
---
+++ peliculas.js
@@ -0,0 +1,38 @@
+module.exports=[
+{   id:"El Señor De Los Anillos",
+    rating:"8/10",
+    awards:["Mejor fotografía "," Mejor banda sonora"," Mejores efectos visuales"," Mejor maquillaje y peluquería"],
+    length:"180 min",
+    price:5000,
+    genre:["Fantástico", " Aventuras", " Acción"]
+},
+{   id:"Matrix",
+    rating:"8/10",
+    awards:["Mejor montaje"," Mejores efectos visuales"," Mejores efectos sonoros"],
+    length:"131 min",
+    price:4500,
+    genre:["Ciencia ficción"," Fantástico", " Acción", " Distopía"]
+},
+
+{   id:"Día de la Independencia",
+    rating:"8/10",
+    awards:["Mejores efectos visuales"],
+    length:"145 min",
+    price:4000,
+    genre:["Ciencia ficción", " Acción"]
+},
+
+{   id:"Titanic",
+    rating:"10/10",
+    awards:["Mejor película"," Mejor director"," Mejor sonido"," Mejor fotografía"],
+    length:"195 min",
+    price:5000,
+    genre:["Romance", "Drama", " Aventuras"]
+},
+{   id:"Volver al futuro",
+    rating:"9/10",
+    awards:["Mejores efectos de sonido"],
+    length:"116 min",
+    price:4500,
+    genre:["Ciencia ficción", " Comedia", " Fantástico", " Aventuras"]
+}]
